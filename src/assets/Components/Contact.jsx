06afaa07bfbca95a04e5c617693d89822825811b
JordanEckford <jordan.eckford@gmail.com>
2023-11-30 13:57:31 +0000

    minor layout adjustments to loading spinner on contact form

--- src/assets/Components/Contact.jsx
+++ src/assets/Components/Contact.jsx
@@ -103,18 +103,20 @@ export const Contact = () => {
     <input
      className={`m-auto mt-5 bg-orange-600 w-[50%] p-2 rounded-md text-white xl:row-start-7 xl:col-span-4 ${sendLoading ? "bg-slate-600" : null}`}
      type="submit"
-     value={"Send"}
+     value={sendLoading ? "Sending..." : "Send"}
      disabled={sendLoading ? true : false}
     />
     {sendLoading ? (
-     <svg className="animate-spin m-auto mt-2 h-5 w-5 text-orange-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-      <path
-       className="opacity-75"
-       fill="currentColor"
-       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
-     </svg>
+     <div className="w-[100%] m-auto col-start-1m col-span-4">
+      <svg className="animate-spin m-auto mt-2 h-5 w-5 text-orange-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+       <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+       ></path>
+      </svg>
+     </div>
     ) : null}
     {isError ? <p className="text-red-500 text-xl text-center font-med">Something went wrong, please try again later!</p> : null}
    </form>
